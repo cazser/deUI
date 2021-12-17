2d44def97f81f7b82140302c66e9c84266bf6a84
cazser <2264712072@qq.com>
2021-12-17 08:57:26 +0800

    webpack首次运行成功

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const path = require('path')
+
+
 module.exports = {
+    mode: "production",
     entry: {
         index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        library: "Hui",
+        libraryTarget: "umd"
     },
     module: {
         rules: [{
