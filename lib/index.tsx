2a4d98ffabb48edfdb2733d933a23c268b4706ad
cazser <2264712072@qq.com>
2021-12-17 13:39:59 +0800

    webpack添加resolve

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom"
-console.log(React, ReactDOM)
+import Button from './Button'
+
+
+
+ReactDOM.render(<Button/>, document.querySelector("#root"))
