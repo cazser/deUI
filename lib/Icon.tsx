fa75b83e5e4573864324a47e39c4b0dc3fd5cd90
cazser <2264712072@qq.com>
2021-12-18 20:17:00 +0800

    引入wechat.svg

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import wechat from './icons/wechat.svg'
 
 interface IconProps{
 	name:string
 }
 const Icon: React.FunctionComponent<IconProps> = (props)=>{
+	console.log(wechat)
 	return (
 		<span>
 			{props.name}
