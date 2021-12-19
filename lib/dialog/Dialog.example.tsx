fd7bd0389296f7a14e57f2e4a4f028e52dc91bdb
cazser <2264712072@qq.com>
2021-12-19 19:23:02 +0800

    创建Dialog组件

---
+++ lib/dialog/Dialog.example.tsx
@@ -0,0 +1,11 @@
+import React
+ from "react";
+ import Dialog from './Dialog'
+
+ export default function(){
+	 return(
+		<div>
+			<Dialog></Dialog>
+		</div>
+	 )
+ }
---
+++ lib/dialog/Dialog.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dialog =()=>{
+	return(
+	<div>
+		对话框
+	</div>
+	)
+}
+
+export default Dialog
