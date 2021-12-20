d21524006c31667713e0c81c3e6523aae28ee3b6
cazser <2264712072@qq.com>
2021-12-20 09:03:28 +0800

    开始完成Dialog了

--- lib/dialog/Dialog.tsx
+++ lib/dialog/Dialog.tsx
@@ -1,3 +1,4 @@
+import Icon from "../Icon";
 import React, { Fragment, ReactChild, ReactFragment, ReactPortal } from "react";
 import './dialog.scss'
 interface Props {
@@ -10,7 +11,19 @@ const Dialog: React.FunctionComponent<Props> =(props)=>{
 		{props.visible? 
 		<Fragment>
 		<div className="deui-dialog-mask"></div>
-		<div className="deui-dialog">{props.children}</div>
+		<div className="deui-dialog">
+			<div className="deui-dialog-close">
+				<Icon name="close" />
+			</div>
+			<header className="deui-dialog-header">提示</header>
+			<main className="deui-dialog-main">
+			{props.children}
+			</main>
+			<footer className="deui-dialog-footer">
+				<button>ok</button>
+				<button>cancel</button>
+			</footer>
+		</div>
 		</Fragment>
 		 : null}
 	</div>
