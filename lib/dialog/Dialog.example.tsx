c1b7ddf51ee057db91636f6c5b42938ca6bfe19e
cazser <2264712072@qq.com>
2021-12-23 14:43:48 +0800

    开始写Layout组件

--- lib/dialog/Dialog.example.tsx
+++ lib/dialog/Dialog.example.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {alert, confirm} from './Dialog'
+import Dialog from './Dialog'
 
  export default function(){
 	 //const [x, setX] = useState(false)
 	 return(
 		<div>
-			<button onClick={()=>{alert("1")}}>alert</button>
-			<button onClick={()=>{confirm("1", ()=>{console.log("你点击了yes")}, 
-											   ()=>{console.log("你点击了no")})}}>confirm</button>
+			<Dialog visible={true} enableMask={false} onClose={()=>{}}>
+				<h1>12</h1>
+			</Dialog>
 		</div>
 	 )
  }
